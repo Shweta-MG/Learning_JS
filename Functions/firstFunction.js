1ac1d80f8dded9974b4e07ecb61ce0aab59044d9
Shweta <shweta.malav@gmail.com>
2023-11-29 12:51:55 +0100

    JS functions are in learning.

--- Functions/firstFunction.js
+++ Functions/firstFunction.js
@@ -5,12 +5,27 @@ const singSong = (singerName) => {
 };
 singSong("Thør");
 
+const singSong1 = (singerName) => {
+    return (`${singerName} is singing a song`)
+  };
+console.log(singSong1("Thør"));
+
 //print heart
 const printHeart = (one, two) => {
   let heart = "<3";
   //console.log(`${one} has ${two}'s hearts`);
 };
 printHeart("Olivia", "Ester");
+
+
+
+const printHeart1 = (one, two) => {
+    let heart = "<3";
+    return (`${one} has ${two}'s hearts`);
+  };
+console.log(printHeart1("Olivia", "Ester"));
+  
+
 
 //print heart three times
 const printHeartManyTimes = (str, times) => {
@@ -27,10 +42,9 @@ const printStringRow = (str, num) => {
     newRow += str;
   }
   console.log(newRow);
-  console.log();
 };
-console.log(printStringRow("I am learning JS ", 6));
-console.log(printStringRow("I am trying hard learning JS ", 6));
+//console.log(printStringRow("I am learning JS ", 6));
+//console.log(printStringRow("I am trying hard learning JS ", 6));
 
 //print average functions
 array1 = [1, 2, 3, 4, 5, 6, 6, 7];
@@ -47,3 +61,54 @@ const average = (array) => {
 };
 //console.log(average(array1));
 //console.log(average(array2));
+;
+
+
+//add numbers and check the values before execution
+arrayStr = [1, 2, 3, 4, 5, 'Olie', 'Kage']
+const add = (array) => {
+    let addStart = 0;
+    for (let i = 0; i < array.length; i++){
+        if (typeof array[i] !== 'number') {
+            return false;
+        }
+        addStart += array[i];        
+    } 
+    return addStart;
+}
+//console.log(add(array1));
+//console.log(add(arrayStr));
+
+
+// return last and second last element of the array
+newStr = [];
+const arrayValue = (array) => {
+    if (array.length === 0) {
+        return 'Null';
+    }
+    let x = array.length - 1;
+    let y = array.length - 2;
+    return (x, y)
+}
+//console.log(arrayValue(array1));
+//console.log(arrayValue(newStr));
+
+
+
+//return day of the week
+const daysOfWeek = ['Null', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const weekDay = (num) => {
+    let weekDayAsked = '';
+    for (let i = 0; i < daysOfWeek.length; i++){
+        if (num === i) {
+            weekDayAsked = daysOfWeek[i];
+            break;             
+        }
+    }
+    return weekDayAsked;
+}
+console.log(weekDay(0));
+console.log(weekDay(2));
+console.log(weekDay(3));
+console.log(weekDay(4));
+console.log(weekDay(5));
