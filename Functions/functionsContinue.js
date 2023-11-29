dce7d0f39da8bb984871dfaf72c350e00479d993
Shweta <shweta.malav@gmail.com>
2023-11-29 17:10:33 +0100

    Functions are being levelling up

--- Functions/functionsContinue.js
+++ Functions/functionsContinue.js
@@ -2,33 +2,75 @@
 let totalWords = 0;
 
 const updateTotalWords = (number) => {
-    totalWords = number;
-    return totalWords;
-}
+  totalWords = number;
+  return totalWords;
+};
 
 //console.log(updateTotalWords(56));
 
 //
-const birdNames = ['Sparrow', 'Robin', 'Blue Jay', 'Cardinal', 'Eagle', 'Hawk', 'Pigeon', 'Crow', 'Hummingbird', 'Owl'];
-let birdName = '';
+const birdNames = [
+  "Sparrow",
+  "Robin",
+  "Blue Jay",
+  "Cardinal",
+  "Eagle",
+  "Hawk",
+  "Pigeon",
+  "Crow",
+  "Hummingbird",
+  "Owl",
+];
+let birdName = "";
 const birdWatch = (number) => {
-    for (let i = 0; i < birdNames.length; i++){
-        if (number === i && number <= birdNames.length) {
-            birdName = birdNames[i];
-            break;            
-        }
-        
-        birdName = 'Invalid index';
-
+  for (let i = 0; i < birdNames.length; i++) {
+    if (number === i && number <= birdNames.length) {
+      birdName = birdNames[i];
+      break;
     }
-    return birdName;
-}
 
-console.log(birdWatch(-1));
-console.log(birdWatch(1));
-console.log(birdWatch(9));
-console.log(birdWatch(11));
+    birdName = "Invalid index";
+  }
+  return birdName;
+};
 
+//console.log(birdWatch(-1));
+//console.log(birdWatch(1));
+//console.log(birdWatch(9));
+//console.log(birdWatch(11));
 
 //functions scope - Block scope
 //functions scope - Lexical scope
+//higher order functions - Func as argument
+
+const rollDie = () => {
+  num = Math.floor(Math.random() * 6) + 1;
+  console.log(num);
+};
+
+const runFunction = (functions) => {
+  rollDie();
+  rollDie();
+};
+
+const runFunctionTenTimes = (functions) => {
+  for (let i = 0; i < 10; i++) {
+    functions();
+  }
+};
+
+//runFunction(rollDie);
+//runFunctionTenTimes(rollDie);
+
+const animals = [
+    {
+        name: "Buddy", type: "Dog", breed: "Golden Retriever", color: "Golden", sound() {
+            return (`${this.name} is a ${this.type} animal. It's breed is ${this.breed} and it is very noiseyy`)
+  } },
+  { name: "Whiskers", type: "Cat", breed: "Siamese", color: "Cream" },
+  { name: "Hoppy", type: "Rabbit", breed: "Holland Lop", color: "Gray" },
+  { name: "Spike", type: "Hedgehog", breed: "African Pygmy", color: "Brown" },
+  { name: "Mittens", type: "Cat", breed: "Persian", color: "White" },
+];
+
+console.log(animals[0].sound());
