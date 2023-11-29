91642a0b7f4cf1aafbc719bf8f338457845e8ae2
Shweta <shweta.malav@gmail.com>
2023-11-29 11:49:54 +0100

    JS functions are in learning.

---
+++ Functions/firstFunction.js
@@ -0,0 +1,15 @@
+//First function
+
+const singSong = () => {
+    console.log('I am singing a song')
+}
+singSong();
+
+//print heart three times
+const printHeart = () => {
+    let heart = '<3'
+    console.log(heart);
+    console.log(heart);
+    console.log(heart)
+}
+printHeart();
