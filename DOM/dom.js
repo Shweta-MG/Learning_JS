4b2f0ff6270864cb9b5afd333c9940d867aabdcf
Shweta <shweta.malav@gmail.com>
2023-12-11 21:31:32 +0100

    JS is in learning

--- DOM/dom.js
+++ DOM/dom.js
@@ -3,5 +3,13 @@ document.querySelector('#about').innerHTML += '<h3> I am updated inned adjacent 
 
 
 //change the id (attribute) using DOM
-document.querySelector('#contact').id = 'contact-details'
+document.querySelector('#contact').id = 'contact-details';
+
+
+//create element than append it
+let h4 = document.createElement('h4')
+h4.innerText = 'I am being appended';
+let h2 = document.querySelector('.section-title')
+h2.appendChild(h4)
+
 
