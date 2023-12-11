d72afff9d779803853cf5140a39974fc34e66ad5
Shweta <shweta.malav@gmail.com>
2023-12-11 21:01:34 +0100

    Js is in learning

---
+++ DOM/dom.js
@@ -0,0 +1,7 @@
+document.querySelector('h1').innerText = 'I am updated inner text';
+document.querySelector('#about').innerHTML += '<h3> I am updated inned adjacent element </h3> <sup>I am testing</sup>'
+
+
+//change the id (attribute) using DOM
+document.querySelector('#contact').id = 'contact-details'
+
